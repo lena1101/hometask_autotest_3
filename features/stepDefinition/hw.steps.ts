d9b368548b270827391542c6f732172d715e7260
Elena1 <rimivaw189@566dh.com>
2021-07-12 19:44:18 +0300

    added "awaits"

--- features/stepDefinition/hw.steps.ts
+++ features/stepDefinition/hw.steps.ts
@@ -34,137 +34,137 @@ export = function exampleSteps() {
 
        
     this.Given(/^I am on demoqa-select page$/, async () => {
-        demoqaPage.navigateToDemoqaSelect();
+        await demoqaPage.navigateToDemoqaSelect();
     });
 
     this.When(/^I click on select$/, async () => {
-        demoqaPage.selectClick();
+        await demoqaPage.selectClick();
     });
 
     this.Then(/^Option must be selected$/, async () => {
-        demoqaPage.selectCheck();
+        await demoqaPage.selectCheck();
     });
 
     this.Given(/^I am on demoqa-checkbox page$/, async () => {
-        demoqaPage.navigateToDemoqaCheckbox();
+        await demoqaPage.navigateToDemoqaCheckbox();
     });
 
     this.When(/^I click on expand button$/, async () => {
-        demoqaPage.buttonClick();
+        await demoqaPage.buttonClick();
     });
 
     this.Then(/^The list must be expanded$/, async () => {
-        demoqaPage.listCheck();
+        await demoqaPage.listCheck();
     });
 
     this.When(/^I click on Note checkbox$/, async () => {
-        demoqaPage.clickCheckboxNotes();
+        await demoqaPage.clickCheckboxNotes();
     });
 
     this.Given(/^I am on demoqa-radiobutton page$/, async () => {
-        demoqaPage.navigateToDemoqaRadiobutton();
+        await demoqaPage.navigateToDemoqaRadiobutton();
     });
 
     this.When(/^I click on radiobutton$/, async () => {
-        demoqaPage.radioButtonClick();
+        await demoqaPage.radioButtonClick();
     });
 
     this.When(/^The value should be -Yes-$/, async () => {
-        demoqaPage.checkValue();
+        await demoqaPage.checkValue();
     });
 
     this.Given(/^I am on demoqa-button page$/, async () => {
-        demoqaPage.navigateToDemoqaButton();
+        await demoqaPage.navigateToDemoqaButton();
     });
 
     this.When(/^I click on button 2 times$/, async () => {
-        demoqaPage.buttonDoubleclick();
+        await demoqaPage.buttonDoubleclick();
     });
 
     this.When(/^The button double-clicked$/, async () => {
-        demoqaPage.clickCheck();
+        await demoqaPage.clickCheck();
     });
 
 
     this.Given(/^I am on demoqa-webtables page$/, async () => {
-        demoqaPage.navigateTodemoqaModal();
+        await demoqaPage.navigateTodemoqaModal();
     });
 
     this.When(/^I click on Add button$/, async () => {
-        demoqaPage.addButtonClick();
+        await demoqaPage.addButtonClick();
     });
 
     this.When(/^The Registration Form is opened$/, async () => {
-        demoqaPage.modalFormOpened();
+        await demoqaPage.modalFormOpened();
     });
 
     this.When(/^I click on Close button$/, async () => {
-        demoqaPage.closeButtonClick();
+        await demoqaPage.closeButtonClick();
     });
 
     this.When(/^The Registration Form is closed$/, async () => {
-        demoqaPage.modalFormClosed();
+        await demoqaPage.modalFormClosed();
     });
 
 
     this.When(/^I enter TEST to firstName field$/, async () => {
-        demoqaPage.fieldFilling1();
+        await demoqaPage.fieldFilling1();
     });
 
     this.When(/^I click on Submit button$/, async () => {
-        demoqaPage.submitButtonClick();
+        await demoqaPage.submitButtonClick();
     });
 
 
     this.When(/^I enter TEST to lastName field$/, async () => {
-        demoqaPage.fieldFilling2();
+        await demoqaPage.fieldFilling2();
     });
     this.When(/^I enter email to userEmail field$/, async () => {
-        demoqaPage.fieldFilling3();
+        await demoqaPage.fieldFilling3();
     });
     this.When(/^I enter 30 to age field$/, async () => {
-        demoqaPage.fieldFilling4();
+        await demoqaPage.fieldFilling4();
     });
     this.When(/^I enter 10000 to salary field$/, async () => {
-        demoqaPage.fieldFilling5();
+        await demoqaPage.fieldFilling5();
     });
     this.When(/^I enter TEST to department field$/, async () => {
-        demoqaPage.fieldFilling6();
+        await demoqaPage.fieldFilling6();
     });
 
 
     this.Given(/^I am on demoqa-frames page$/, async () => {
-        demoqaPage.navigateToDemoqaIframes();
+        await demoqaPage.navigateToDemoqaIframes();
     });
 
     this.When(/^I switch to Iframe$/, async () => {
-        demoqaPage.iframeSwitchTo();
+        await demoqaPage.iframeSwitchTo();
     });
 
     this.When(/^I see the title - This is a sample page$/, async () => {
-        demoqaPage.titleCheck();
+        await demoqaPage.titleCheck();
     });
 
 
 
     this.Given(/^I am on Ozon page$/, async () => {
-        ozonPage.navigateToOzonByURL();
+        await ozonPage.navigateToOzonByURL();
     });
 
     this.When(/^I click on Ozone Travel button$/, async () => {
-        ozonPage.headerLinkClick();
+        await ozonPage.headerLinkClick();
     });
 
     this.When(/^I see Ozone Travel logo$/, async () => {
-        ozonPage.checkValue();
+        await ozonPage.checkValue();
     });
 
     this.When(/^I click on search -from where- field$/, async () => {
-        ozonPage.searchFieldClick();
+        await ozonPage.searchFieldClick();
     });
 
     this.When(/^The list of cities is opened$/, async () => {
-        ozonPage.checkList();
+        await ozonPage.checkList();
     });
 
     // this.When(/^I click on Ozon Card button$/, async () => {
@@ -185,57 +185,57 @@ export = function exampleSteps() {
 
 
        this.When(/^I click on Catalog Menu button$/, async () => {
-        ozonPage.catalogMenuButtonClick();
+        await ozonPage.catalogMenuButtonClick();
     });
 
     this.When(/^I navigate to Sport-i-otdyh option$/, async () => {
-        ozonPage.sportRestOption();
+        await ozonPage.sportRestOption();
     });
 
     this.When(/^I see Decatlon logo$/, async () => {
-        ozonPage.decatlonLogo();
+        await ozonPage.decatlonLogo();
     });
 
 
 
     this.Given(/^I am on Amazon page$/, async () => {
-        amazonPage.navigateToAmazonByURL();
+        await amazonPage.navigateToAmazonByURL();
     });
 
     this.When(/^I click on Amazon Business Card link$/, async () => {
-        amazonPage.footerLinkClick();
+        await amazonPage.footerLinkClick();
     });
 
     this.When(/^I see Apply now button$/, async () => {
-        amazonPage.checkValue();
+        await amazonPage.checkValue();
     });
 
     this.When(/^I click on Apply now button$/, async () => {
-        amazonPage.applyNowButtonClick();
+        await amazonPage.applyNowButtonClick();
     });
     this.When(/^The page with registration form is opened$/, async () => {
-        amazonPage.checkSignInForm();
+        await amazonPage.checkSignInForm();
     });
 
 
     this.When(/^I enter Camera & Photo in search-bar-form$/, async () => {
-        amazonPage.fastSearchSendKeys();
+        await amazonPage.fastSearchSendKeys();
     });
 
     this.When(/^I push on Enter button$/, async () => {
-        amazonPage.pressEnter();
+        await amazonPage.pressEnter();
     });
 
 
     this.When(/^I click on dropdown Sort by button$/, async () => {
-        amazonPage.dropDownButtonClick();
+        await amazonPage.dropDownButtonClick();
     });
     this.When(/^I see Price: Low to High option in the DDL$/, async () => {
-        amazonPage.lowToHighOption();
+        await amazonPage.lowToHighOption();
     });
 
     this.Given(/^I am on Newyorker page$/, async () => {
-        newyorkerPage.navigateToNewyorkerByURL();
+        await newyorkerPage.navigateToNewyorkerByURL();
     });
 
     this.When(/^enter "(.*?)" in Search field$/, async (input: string) => {
@@ -249,52 +249,50 @@ export = function exampleSteps() {
 
 
     this.Given(/^I am on Newyorker main page$/, async () => {
-        newyorkerPage.navigateToNewyorkerMain();
+        await newyorkerPage.navigateToNewyorkerMain();
     });
 
     this.When(/^I click on Newsletter link$/, async () => {
-        newyorkerPage.headerLinkClick();
+        await newyorkerPage.headerLinkClick();
     });
 
     this.When(/^I click on The Daily checkbox$/, async () => {
-        newyorkerPage.checkboxClick();
+        await newyorkerPage.checkboxClick();
     });
 
     this.When(/^I enter test word to Your e-mail address field$/, async () => {
-        newyorkerPage.signUpFieldFill();
+        await newyorkerPage.signUpFieldFill();
     });
     this.When(/^I see wrong validation status$/, async () => {
-        newyorkerPage.validationCheck();
+        await newyorkerPage.validationCheck();
     });
 
 
 
     this.Given(/^I am on developer-mozilla page$/, async () => {
-        mozillaPage.navigateToMozillaSelect();
+        await mozillaPage.navigateToMozillaSelect();
     });
 
     this.When(/^I click on Change your language DDL$/, async () => {
-        mozillaPage.selectClick();
+        await mozillaPage.selectClick();
     });
 
     this.When(/^I select Polski option$/, async () => {
-        mozillaPage.selectCheck();
+        await mozillaPage.selectCheck();
     });
     this.When(/^I click on Change language button$/, async () => {
-        mozillaPage.buttonClick();
+        await mozillaPage.buttonClick();
     });
     this.When(/^I see Polish title$/, async () => {
-        mozillaPage.titleCheck();
+        await mozillaPage.titleCheck();
     });
 
     this.When(/^I click on View in English button$/, async () => {
-        mozillaPage.changeButton();
+        await mozillaPage.changeButton();
     });
     this.When(/^I see English title$/, async () => {
-        mozillaPage.title2Check();
+        await mozillaPage.title2Check();
     });
-
-
 
 
 }
